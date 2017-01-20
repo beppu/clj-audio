eb3528cae70c44bfabc4378e996155304d11fb54
John BEPPU <john.beppu@gmail.com>
2017-01-20 13:28:36 -0800

    Remove :dev-dependencies and bump clojure to 1.7.0

--- project.clj
+++ project.clj
@@ -1,6 +1,3 @@
-(defproject clj-audio/clj-audio "0.3.0-SNAPSHOT"
+(defproject clj-audio/clj-audio "0.3.0"
   :description "Idiomatic Clojure wrapper for the Java Sound API."
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [swank-clojure "1.2.1"]
-                     [clj-help "0.1.1"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]])
