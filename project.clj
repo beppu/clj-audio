8ea5237c3c52a59d08b21adb23745a2805dc09df
John BEPPU <john.beppu@gmail.com>
2017-01-20 13:59:29 -0800

    Add :url and use org.clojars.beppu as group

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject clj-audio/clj-audio "0.3.0"
+(defproject org.clojars.beppu/clj-audio "0.3.0"
   :description "Idiomatic Clojure wrapper for the Java Sound API."
+  :url "https://github.com/beppu/clj-audio"
   :dependencies [[org.clojure/clojure "1.7.0"]])
